2562ca9ead517fd98ededf12c20c202d739a3d0c
Evgeny Norkin <norkinzheny@gmail.com>
2023-03-15 14:39:36 +0300

    Mistakes are fixed.

--- src/main/java/SQL_createTable/SQL.java
+++ src/main/java/SQL_createTable/SQL.java
@@ -6,6 +6,8 @@ import org.apache.logging.log4j.Logger;
 
 import java.sql.*;
 
+import static java.sql.DriverManager.getConnection;
+
 /*
 Написать скрипт по созданию 2 таблиц: author(id,name,surname), book(id, title, authod_id, pages).
 Заполнить таблицы данными(через скрипт)
@@ -21,19 +23,7 @@ public class SQL {
     private static String PASSWORD = "root";
     private static final Logger logger = LogManager.getLogger(Main.class);
 
-    private static Connection getConnection() throws SQLException {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-            logger.info("Connection succesfull!");
-        } catch (Exception ex) {
-            System.out.println("Connection failed...");
-            logger.error("Connection failed..." + ex);
-        }
-        return DriverManager.getConnection(URL, LOGIN, PASSWORD);
-    }
-
     protected void initConn() throws SQLException {
-        getConnection();
         createTableAuthor();
         createTableBook();
         insertValuesIntoTableAuthor();
@@ -46,19 +36,18 @@ public class SQL {
         Connection connection = null;
         Statement stm = null;
         try {
-            connection = getConnection();
+            connection = getConnection(URL, LOGIN, PASSWORD);
             connection.setAutoCommit(false);
-            Statement statement = connection.createStatement();
+            stm = connection.createStatement();
             String sql = "CREATE TABLE author (id INT PRIMARY KEY , name TEXT(10), surname TEXT(10))";
-            statement.executeUpdate(sql);
+            stm.executeUpdate(sql);
             connection.commit();
-
         } catch (SQLException | RuntimeException e) {
             System.err.println(e);
             logger.error(e);
         } finally {
-            stm.close();
             connection.close();
+            stm.close();
         }
     }
 
@@ -66,19 +55,18 @@ public class SQL {
         Connection connection = null;
         Statement stm = null;
         try {
-            connection = getConnection();
+            connection = getConnection(URL, LOGIN, PASSWORD);
             connection.setAutoCommit(false);
             stm = connection.createStatement();
             String sql = "CREATE TABLE book(id INT PRIMARY KEY, title TEXT(10),author_id INT, pages INT)";
             stm.executeUpdate(sql);
             connection.commit();
-
         } catch (SQLException | RuntimeException e) {
             System.err.println(e);
             logger.error(e);
         } finally {
-            stm.close();
             connection.close();
+            stm.close();
             logger.info("Resources are closed.");
         }
     }
@@ -87,7 +75,7 @@ public class SQL {
         Connection connection = null;
         Statement stm = null;
         try {
-            connection = getConnection();
+            connection = getConnection(URL, LOGIN, PASSWORD);
             connection.setAutoCommit(false);
             stm = connection.createStatement();
             String sql = "INSERT INTO author (id, name ,surname) VALUES (1,'Сергей','Есенин')," +
@@ -95,13 +83,12 @@ public class SQL {
                     "(3,'Александр','Пушкин')";
             stm.executeUpdate(sql);
             connection.commit();
-
         } catch (SQLException | RuntimeException e) {
             System.err.println(e);
             logger.error(e);
         } finally {
-            stm.close();
             connection.close();
+            stm.close();
             logger.info("Resources are closed.");
         }
     }
@@ -110,7 +97,7 @@ public class SQL {
         Connection connection = null;
         Statement stm = null;
         try {
-            connection = getConnection();
+            connection = getConnection(URL, LOGIN, PASSWORD);
             connection.setAutoCommit(false);
             stm = connection.createStatement();
             String sql = "INSERT INTO book (id, title, author_id, pages) VALUES (1,'Письмо к женщине',1,100)," +
@@ -118,13 +105,12 @@ public class SQL {
                     "(3,'Зимнее утро',3,245)";
             stm.executeUpdate(sql);
             connection.commit();
-
         } catch (SQLException | RuntimeException e) {
             System.err.println(e);
             logger.error(e);
         } finally {
-            stm.close();
             connection.close();
+            stm.close();
             logger.info("Resources are closed.");
         }
     }
@@ -134,7 +120,7 @@ public class SQL {
         Statement stm = null;
         ResultSet rs = null;
         try {
-            connection = getConnection();
+            connection = getConnection(URL, LOGIN, PASSWORD);
             connection.setAutoCommit(false);
             stm = connection.createStatement();
             rs = stm.executeQuery("select title AS Book, pages AS Pages from book\n" +
@@ -143,14 +129,13 @@ public class SQL {
                 String str = rs.getString("Book") + " " + rs.getString("Pages");
                 System.out.println(str);
             }
-
         } catch (SQLException | RuntimeException e) {
             System.err.println(e);
             logger.error(e);
         } finally {
-            connection.close();
-            stm.close();
             rs.close();
+            stm.close();
+            connection.close();
             logger.info("Resources are closed.");
         }
     }
@@ -160,24 +145,24 @@ public class SQL {
         Statement stm = null;
         ResultSet rs = null;
         try {
-            connection = getConnection();
+            connection = getConnection(URL, LOGIN, PASSWORD);
             connection.setAutoCommit(false);
             stm = connection.createStatement();
             rs = stm.executeQuery("select a1.name AS Author, a1.surname, a2.title AS Book from author a1\n" +
                     "join book a2 on a1.id = a2.author_id\n" +
                     "where a2.pages > 100");
             while (rs.next()) {
-                String str = rs.getString("Author") + " " + rs.getString("surname") + " " + rs.getString("Book");
+                String str = rs.getString("Author") + " " + rs.getString("surname") + " "
+                        + rs.getString("Book");
                 System.out.println(str);
             }
-
         } catch (SQLException | RuntimeException e) {
             System.err.println(e);
             logger.error(e);
         } finally {
-            connection.close();
-            stm.close();
             rs.close();
+            stm.close();
+            connection.close();
             logger.info("Resources are closed.");
         }
     }
